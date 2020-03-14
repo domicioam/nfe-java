5557436f38446c9e2ffefd1a2953082d57acbdbf
Domício Medeiros <domicioam@gmail.com>
2020-03-14 17:57:45 +0000

    Unit testing certificate upload.
    Adding unit test to validate file upload. Adding method signature in
    CertificadoController.

--- nfeservice/src/main/java/com/dgsystems/nfeservice/cadastro/controllers/CertificadoController.java
+++ nfeservice/src/main/java/com/dgsystems/nfeservice/cadastro/controllers/CertificadoController.java
@@ -1,5 +1,22 @@
 package com.dgsystems.nfeservice.cadastro.controllers;
 
-public class CertificadoController {
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@Validated
+@RequestMapping("/empresa/{empresaId}/certificado")
+public class CertificadoController {
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void save(@PathVariable("empresaId") Integer empresaId, @RequestParam("certificado") MultipartFile certificado) {
+		
+	}
 }
--- nfeservice/src/test/java/com/dgsystems/nfeservice/CertificadoControllerTest.java
+++ nfeservice/src/test/java/com/dgsystems/nfeservice/CertificadoControllerTest.java
@@ -1,14 +1,53 @@
 package com.dgsystems.nfeservice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.dgsystems.nfeservice.cadastro.controllers.CertificadoController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
 class CertificadoControllerTest {
+	
+	ObjectMapper mapper;
+	MockMvc mvc;
+	
+	@InjectMocks
+	CertificadoController certificadoController;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void shouldSaveUploadedCertificado() throws Exception {
+		File file = new File("src/test/resources/MyDevCert.pfx");
+		InputStream inputStream = new FileInputStream(file);
+	    MockMultipartFile certificado = new MockMultipartFile("certificado", "MyDevCert.pfx", "application/x-pkcs12", inputStream);
+		
+		mvc.perform(multipart("/empresa/1/certificado/").file(certificado))
+			.andExpect(status().isCreated())
+			.andReturn();
+	}
+	
+	@BeforeAll
+	void setUp() {
+		mapper = new ObjectMapper();
+		mvc = MockMvcBuilders.standaloneSetup(certificadoController).build();
 	}
 
 }
